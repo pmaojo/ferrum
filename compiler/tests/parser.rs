47259528cfa73bcf9f6ed700fa863a2be3d6b960
Pelayo Maojo <pelayo.mao@gmail.com>
2025-06-28 12:16:25 +0100

    feat: add semantic fields and docs generation

--- compiler/tests/parser.rs
+++ compiler/tests/parser.rs
@@ -7,6 +7,11 @@ fn parse_simple_yaml() {
 nodes:
   - id: ping
     type: usecase
+    description: Simple ping use case
+    story: |
+      As a user
+      I want to ping the service
+      So that I know it is alive
 "#;
     let dir = tempfile::tempdir().unwrap();
     let file = dir.path().join("test.yaml");
@@ -15,4 +20,13 @@ nodes:
     assert_eq!(module.name, "test");
     assert_eq!(module.nodes.len(), 1);
     assert_eq!(module.nodes[0].id, "ping");
+    assert_eq!(
+        module.nodes[0].description.as_deref(),
+        Some("Simple ping use case")
+    );
+    assert!(module.nodes[0]
+        .story
+        .as_ref()
+        .unwrap()
+        .contains("ping the service"));
 }
