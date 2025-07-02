c33a5fb9d4c7e87246430a32780538c76857f7e8
Pelayo Maojo <pelayo.mao@gmail.com>
2025-07-02 15:09:13 +0100

    Add log viewer to desktop studio

--- studio-desktop/src/python.rs
+++ studio-desktop/src/python.rs
@@ -1,5 +1,5 @@
-use std::process::{Child, Command};
 use std::path::PathBuf;
+use std::process::{Child, Command, Stdio};
 
 pub fn start_python_service() -> std::io::Result<Child> {
     let mut ai_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
@@ -7,5 +7,7 @@ pub fn start_python_service() -> std::io::Result<Child> {
     Command::new("uvicorn")
         .current_dir(ai_path)
         .args(["main:app", "--host", "0.0.0.0", "--port", "8000"])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .spawn()
 }
